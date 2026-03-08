db52f196c7e18c98923956003093193be25a40f9
OnSwitchOff <kassov@ukr.net>
2026-03-08 10:10:24 +0200

    QueryRunner pattern Global ZOD exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {queryLogger} from "./db/query-counter.logger";
+import { ZodExceptionFilter } from './zod-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use((req, res, next) => {
     queryLogger.queryCount = 0;
-    res.on("finish", () => {
-      console.log("SQL queries executed:", queryLogger.queryCount);
+    res.on('finish', () => {
+      console.log('SQL queries executed:', queryLogger.queryCount);
     });
     next();
   });
+  app.useGlobalFilters(new ZodExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3001);
 }
